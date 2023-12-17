9bf07006bc6735b01135d4a8300ead430fb0fca7
Kur04ka <vanya.kurochkin.23@mail.ru>
2023-12-17 21:14:52 +0700

    first commit

---
+++ WpfApp/Model/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private string name, faculty;
+        private int age;
+        
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public string Faculty
+        {
+            get { return faculty; }
+            set
+            {
+                faculty = value;
+                OnPropertyChanged("Faculty");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ WpfApp/ViewModel/ViewModelEmloyee.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfApp.ViewModel;
+
+namespace WpfApp
+{
+    public class ViewModelEmloyee : INotifyPropertyChanged
+    {
+        public ObservableCollection<Employee> Employees { get; set; }
+        public ViewModelEmloyee()
+        {
+            Employees = new ObservableCollection<Employee>()
+            {
+                new Employee() {Name="Ivan Kurochkin", Age=33, Faculty="Applied Informatics"},
+                new Employee() {Name="Trent Alexander", Age=49, Faculty="Informational Systems"},
+                new Employee() {Name="Oleg Nikitin", Age=61, Faculty="Programming"},
+                new Employee() {Name="Cristiano Ronaldo", Age=25, Faculty="Applied Physics"}
+            };
+        }
+
+
+        #region SelectedItem and NewEmployee propeties 
+
+        /// <summary>
+        /// Selected item property which contains clicked item
+        /// </summary>
+        private Employee selectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get { return selectedEmployee; }
+            set
+            {
+                selectedEmployee = value;
+                OnPropertyChanged("SelectedEmployee");
+            }
+        }
+
+        /// <summary>
+        /// New emlpoyee propety which contains fields of Employee Model
+        /// </summary>
+        private Employee newEmployee = new Employee();
+        public Employee NewEmployee
+        {
+            get { return newEmployee; }
+            set
+            {
+                newEmployee = value;
+                OnPropertyChanged("NewEmployee");
+            }
+        }
+
+        #endregion
+
+
+        #region Add and Remove commands
+
+        private RelayCommand addEmployeeComand;
+        public RelayCommand AddEmployeeComand
+        {
+            get
+            {
+                return addEmployeeComand ?? (addEmployeeComand = new RelayCommand(obj =>
+                {
+                    Employee employee = new Employee() { Name = NewEmployee?.Name, Age = (int)(NewEmployee?.Age), Faculty = NewEmployee?.Faculty };
+                    Employees.Insert(0, employee);
+                    selectedEmployee = employee;
+                }));
+            }
+        }
+
+        private RelayCommand removeEmployeeCommand;
+        public RelayCommand RemoveEmployeeCommand
+        {
+            get
+            {
+                return removeEmployeeCommand ?? (removeEmployeeCommand = new RelayCommand(obj =>
+                {
+                    Employee employee = (Employee)obj;
+                    if (employee != null)
+                    {
+                        Employees.Remove(employee);
+                    }
+                },
+                obj => Employees.Count > 0));
+            }
+        }
+
+        #endregion
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        #endregion
+
+
+    }
+}
